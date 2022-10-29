9537bb5d10aa62726375218e35def472545a7570
vensen002 <vensen002@outlook.com>
2022-10-30 01:56:11 +0800

    update

---
+++ assets/index.html.e8687502.js
@@ -0,0 +1,18 @@
+import{_ as e,o as i,c as l,a}from"./app.a80cbf96.js";const d={},n=a(`<h1 id="markdown-基础语法" tabindex="-1"><a class="header-anchor" href="#markdown-基础语法" aria-hidden="true">#</a> markdown 基础语法</h1><ul><li><em>斜体</em></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>*Italic*
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li><strong>粗体</strong></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>**Bold**
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li>#标题</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code># heading 
+## heading
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><ul><li><a href="#">链接</a></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>[Link](http://a.com)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li><code>![图片](/images/markdown.png)</code></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>![Image](http://url/a.png)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li><blockquote><p>Blockquote</p></blockquote></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>&gt; Blockquote
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li>*List 无序列表</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>* List
+* List
+* List
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li><ol><li><ol start="2"><li><ol start="3"><li>有序列表</li></ol></li></ol></li></ol></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. One
+2. Two
+3. Three
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li>分割线</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>---
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li><code>行内代码块</code></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>\`行内代码块\`
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li>代码块</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>以 \`\`\` 开始 [文本类型]
+以 \`\`\` 结束
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div>`,23),s=[n];function t(r,u){return i(),l("div",null,s)}const v=e(d,[["render",t],["__file","index.html.vue"]]);export{v as default};
